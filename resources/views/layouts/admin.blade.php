74abc734a310956f258b8e3dd2de4ea70ea4c734
daniel <daniele333kx@gmail.com>
2022-09-29 15:18:13 +0500

    add admin-menu after removing

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -30,21 +30,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
                 <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <a href="{{ route('home') }}" class="nav-link">Главная</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('admin.index') }}" class="nav-link">Главная админка</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('admin.news.create') }}" class="nav-link">Добавить новость</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('admin.test1') }}" class="nav-link">test1</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('admin.test2') }}" class="nav-link">test2</a>
-                    </li>
+                    @yield('menu')
                 </ul>
 
                 <!-- Right Side Of Navbar -->
