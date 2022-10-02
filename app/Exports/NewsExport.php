16d7f22137e201fb025b59022916ebc0e59a24bf
daniel <daniele333kx@gmail.com>
2022-10-02 23:34:58 +0500

    excel is complete

--- app/Exports/NewsExport.php
+++ app/Exports/NewsExport.php
@@ -3,14 +3,15 @@
 namespace App\Exports;
 
 use App\Models\News;
+use Maatwebsite\Excel\Concerns\FromArray;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
-class NewsExport implements FromCollection
+class NewsExport implements FromArray
 {
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function collection()
+    public function array(): array
     {
         return News::all();
     }
